d07b81093bd4523f2fc5945341ba380d1b3d132c
Amanda Wixted <amanda@amandawixted.com>
2018-04-22 17:27:44 -0400

    turn the outboard pixel on blue too

--- mermaid_onboard_pixels.py
+++ mermaid_onboard_pixels.py
@@ -9,6 +9,9 @@ import neopixel
 
 
 pixels = neopixel.NeoPixel(board.NEOPIXEL, 10, brightness= 0.05) #determine beightness (Value can be between 0 and 1)
+outboards = neopixel.NeoPixel(board.A2, 1, brightness=0.5)
+
+
 
 blues = [(0,51,51), (0,102,102), (0,153,153), (0,204,204), (0,255,255), (51,255,255), (102,255,255), (153,255,255), (204,255,255)]
 reds = [(153,0,0)]
@@ -17,7 +20,7 @@ curPixel = 0
 while True:
     print("on pixel ", curPixel)
     bluesCount = 0
-    
+    outboards[0] = blues[0]
     while bluesCount < len(blues):
         pixels[curPixel] = blues[bluesCount]
         time.sleep(0.1)
