d52d07cfc5e76a7787fd349f9a01167cca8c9532
Amanda Wixted <amanda@amandawixted.com>
2018-04-26 16:03:57 -0400

    got a stable refresh rate

--- april_project.py
+++ april_project.py
@@ -4,86 +4,59 @@ import board
 import neopixel
 from digitalio import DigitalInOut, Direction, Pull
 import audioio
+from random import randint
 
-# enable the speaker
-#speakerEnable = DigitalInOut(board.SPEAKER_ENABLE)
-#speakerEnable.direction = Direction.OUTPUT
-#speakerEnable.value = True
+def smart_delay(delay: float, last_time: float) -> float:
+    """
+    A "smart" delay mechanism which tries to reduce the
+    delay as much as possible based on the time the last
+    delay happened.
 
-# make the 2 input buttons
-#buttonA = DigitalInOut(board.BUTTON_A)
-#buttonA.direction = Direction.INPUT
-#button.pull = Pull.DOWN
-#buttonB = DigitalInOut(board.BUTTON_B)
-#buttonB.direction = Direction.INPUT
-#buttonB.pull = Pull.DOWN
+    :param delay: delay in seconds
+    :param last_cmd: time of last command
+    :param remain: counter, skip delay unless it's zero
 
-audiofiles = ["rimshot.wav"]
+    :return: timestamp to feed to next invocation
+    """
+    now = time.monotonic()
+    if delay > 0.0:
 
-def play_file(filename):
-    print("playing file " + filename)
-    f = open(filename, "rb")
-    a = audioio.AudioOut(board.SPEAKER, f)
-    a.play()
-    while a.playing:
-        pass
-    print("finished")
-    
-print("start")
+        delta = now - last_time
+        if delta < delay:
+            sleep = delay - delta
+            time.sleep(sleep)
 
-# set up note values in Hz. Find frequency values at https://pages.mtu.edu/~suits/notefreqs.html
-Ab3 = 208
-A3 = 223
-As3 = 233
-Bb3 = 233
-B3 = 247
-C4 = 262
-Cs4 = 277
-Db4 = 277
-D4 = 294
-Ds4 = 311
-Eb4 = 311
-E4 = 330
-F4 = 349
-Fs4 = 370
-Gb4 = 370
-G4 = 392
-Gs4 = 415
-A4 = 440
-B4 = 494
-C5 = 523
+    return time.monotonic() 
 
+#def setColor():
 
-#onboardPixels = neopixel.NeoPixel(board.NEOPIXEL, 10, auto_write=False)
-
-outboardPixels = neopixel.NeoPixel(board.A1, 120, brightness=0.05, auto_write=False)
-
-pixels = outboardPixels
-
-curColor = []
-for i in range(len(pixels)):
-    curColor.append(0)
-
-bluesStart = [(0,51,51), (0,102,102), (0,153,153), (0,204,204), (0,255,255), (51,255,255), (102,255,255), (153,255,255), (204,255,255)]
-# sentry-march the blues
-bluesCopy = bluesStart.copy()
-bluesStart.reverse()
-blues = bluesCopy + bluesStart
-
-
-def clearPixels():
+def clearPixels(pixels):
     for i in range(len(pixels)):
         pixels[i] = (0,0,0)
     pixels.show()
 
+def main():
 
-clearPixels()
-count = 0
-while True:
-    print("yo ",count)
-    for i in range(len(outboardPixels)):
-        outboardPixels[i] = blues[count]
-    pixels.show()
-    count = (count + 1) % len(blues)
-    time.sleep(0.1)
+    print("start")
+
+    onboardPixels = neopixel.NeoPixel(board.NEOPIXEL, 10, auto_write=False)
+
+    outboardPixels = neopixel.NeoPixel(board.A1, 120, brightness=0.05, auto_write=False)
+
+    pixels = outboardPixels
+
+    clearPixels(pixels)
+
+    tick_time = time.monotonic()
+    count = 0
+    while True:
+        count += 1
+        for i in range(len(outboardPixels)):
+            outboardPixels[i] = (randint(0,255),randint(0,255),randint(0,255))
+        pixels.show()
+        tick_time = smart_delay(0.1, tick_time)
     
+    
+    
+
+main()
---
+++ music_notes.py
@@ -0,0 +1,24 @@
+# music notes in Hz
+
+
+# set up note values in Hz. Find frequency values at https://pages.mtu.edu/~suits/notefreqs.html
+Ab3 = 208
+A3 = 223
+As3 = 233
+Bb3 = 233
+B3 = 247
+C4 = 262
+Cs4 = 277
+Db4 = 277
+D4 = 294
+Ds4 = 311
+Eb4 = 311
+E4 = 330
+F4 = 349
+Fs4 = 370
+Gb4 = 370
+G4 = 392
+Gs4 = 415
+A4 = 440
+B4 = 494
+C5 = 523
