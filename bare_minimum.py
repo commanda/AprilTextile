91cef6eb652013dee673f5dedbf96b0588b33d15
Amanda Wixted <amanda@amandawixted.com>
2018-07-10 15:16:08 -0400

    added busio

--- bare_minimum.py
+++ bare_minimum.py
@@ -1,9 +1,16 @@
-from board import A1
+import board
 import neopixel
 import time
+#import adafruit_ds3231
+import busio
 
-outboards = neopixel.NeoPixel(A1, 120, brightness=0.05, auto_write=False)
+outboards = neopixel.NeoPixel(board.A1, 120, brightness=0.05, auto_write=False)
 color = (255,255,30)
+
+
+# initialize the I2C bus
+myI2C = busio.I2C(board.SCL, board.SDA)
+
 while True:
     print("voila!")
     for i in range(len(outboards)):
