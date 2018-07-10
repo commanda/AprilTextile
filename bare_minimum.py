3d3cb7eacd56ecd272208251d63fd51e13686018
Amanda Wixted <amanda@amandawixted.com>
2018-07-10 15:18:03 -0400

    added the rtc

--- bare_minimum.py
+++ bare_minimum.py
@@ -1,7 +1,7 @@
 import board
 import neopixel
 import time
-#import adafruit_ds3231
+import adafruit_ds3231
 import busio
 
 outboards = neopixel.NeoPixel(board.A1, 120, brightness=0.05, auto_write=False)
@@ -11,8 +11,15 @@ color = (255,255,30)
 # initialize the I2C bus
 myI2C = busio.I2C(board.SCL, board.SDA)
 
+# create the rtc object to talk to the rtc board
+rtc = adafruit_ds3231.DS3231(myI2C)
+
 while True:
     print("voila!")
+    t = rtc.datetime
+    
+    print(t.tm_hour, t.tm_min, t.tm_sec)
+    
     for i in range(len(outboards)):
         outboards[i] = color
     outboards.show()
