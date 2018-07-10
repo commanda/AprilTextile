948681fddf26e13f068a6b3718faf1ba131032b3
Amanda Wixted <amanda@amandawixted.com>
2018-07-10 15:29:48 -0400

    hardcode the len of pixels

--- bare_minimum.py
+++ bare_minimum.py
@@ -4,6 +4,7 @@ from time import sleep
 import adafruit_ds3231
 import busio
 
+num_leds = 120
 
 def normalize(x, old_min, old_max, new_min, new_max):
     return (((x - old_min) / (old_max - old_min)) * (new_max - new_min)) + new_min
@@ -11,10 +12,10 @@ def normalize(x, old_min, old_max, new_min, new_max):
 def put_value_into_pixels_range(x, max, num_leds):
     return num_leds - floor(normalize(x, 0, max, 0, num_leds-1)) - 1
 
-outboards = neopixel.NeoPixel(board.A1, 120, brightness=0.05, auto_write=False)
+outboards = neopixel.NeoPixel(board.A1, num_leds, brightness=0.05, auto_write=False)
 
 
-for i in range(len(outboards)):
+for i in range(num_leds):
     outboards[i] = (255,255,0)
     
     
