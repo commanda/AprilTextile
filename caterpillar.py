9ee9f587d3ee36fa0040e6608416048edcb2918f
Amanda Wixted <amanda@amandawixted.com>
2018-07-10 16:57:14 -0400

    three caterpillars

--- caterpillar.py
+++ caterpillar.py
@@ -7,35 +7,41 @@ num_leds = 120
 
 outboards = neopixel.NeoPixel(board.A1, num_leds, brightness=0.05, auto_write=False)
 
-chosen = 110
-chosen_color = (0,0,255)
-secondary_chosen_color = (82, 143, 242)
+chosen_blue = 110
+blue = (0,0,255)
+light_blue = (82, 143, 242)
 
-def stamp_caterpillar(around_index, size):
+chosen_red = chosen_blue - 35
+red = (249, 124, 22)
+orange = (247, 199, 27)
+
+chosen_white = chosen_red - 50
+white = (255,255,255)
+dark_blue = (9, 2, 132)
+
+def stamp_caterpillar(around_index, size, primary, secondary):
     start = floor(around_index - (size/2))
     stop = floor(around_index + (size/2)) 
     k = 0
     for i in range(start, stop, 1):
-        color = chosen_color
+        color = primary
         if (k < size/3 or k > (2*(size/3))):
-            color = secondary_chosen_color
+            color = secondary
         outboards[i % num_leds]    = color
         k = k+1
     
-    #outboards[(chosen-1) % num_leds]    = chosen_color
-    #outboards[chosen]                   = chosen_color
-    #outboards[(chosen+1) % num_leds]    = chosen_color
-    
-    #outboards[(chosen+2) % num_leds]    = secondary_chosen_color
-    #outboards[(chosen+3) % num_leds]    = secondary_chosen_color
 
 while True:
     for i in range(num_leds):
-        outboards[i] = (255,255,0)
+        outboards[i] = (0,0,0)
     
-    chosen = (chosen + 1) % num_leds
+    chosen_blue = (chosen_blue + 1) % num_leds
+    chosen_red = (chosen_red + 1) % num_leds
+    chosen_white = (chosen_white +1) % num_leds
 
-    stamp_caterpillar(chosen, 12)
+    stamp_caterpillar(chosen_blue, 12, blue, light_blue)
+    stamp_caterpillar(chosen_red, 20, orange, red)
+    stamp_caterpillar(chosen_white, 30, white, dark_blue)
     
     outboards.show()
     sleep(0.01)
