60d0f57e6d4843358efb971a4dd10ffcc6a5726d
Amanda Wixted <amanda@amandawixted.com>
2018-07-11 17:59:25 -0400

    print the grayscale value of each pixel in the image

--- perlin.py
+++ perlin.py
@@ -1,6 +1,28 @@
 from PIL import Image
-jpgfile = Image.open("grayscale.jpg")
+f = Image.open("grayscale.jpg")
+pix = f.load()
 
-print(jpgfile.bits, jpgfile.size, jpgfile.format)
+print(f.bits, f.size, f.format)
 
-print("hello")
+array = []
+
+for i in range(f.size[0]):
+    for j in range(f.size[1]):
+        pixel = pix[i,j]
+        # blue =  pixel & 255
+        # green = (pixel >> 8) & 255
+        # red =   (pixel >> 16) & 255
+        # alpha = (pixel >> 24) & 255
+        # print(pixel)
+        # print(red, green, blue, alpha)
+        # there's only stuff in the "blue" channel, which is the grayscale value
+        array.append(pixel)
+        
+
+print(array)
+print("done")
+
+
+
+def normalize(x, old_min, old_max, new_min, new_max):
+    return (((x - old_min) / (old_max - old_min)) * (new_max - new_min)) + new_min
