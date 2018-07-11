e844016bde5e2fe407a7f8743605833658fbbb9f
Amanda Wixted <amanda@amandawixted.com>
2018-07-11 13:20:28 -0400

    picked better colors

--- bare_minimum.py
+++ bare_minimum.py
@@ -13,7 +13,7 @@ def stamp_caterpillar(around_index, size, primary, secondary):
     k = 0
     for i in range(start, stop, 1):
         color = primary
-        if (k < size/3 or k > (2*(size/3))):
+        if (k < size/3 or k >= (2*(size/3))):
             color = secondary
         outboards[i % num_leds]    = color
         k = k+1
@@ -24,7 +24,7 @@ def normalize(x, old_min, old_max, new_min, new_max):
 def put_value_into_pixels_range(x, max, num_leds):
     return floor(normalize(x, 0, max, 0, num_leds-1)) - 1
 
-outboards = neopixel.NeoPixel(board.A1, num_leds, brightness=0.05, auto_write=False)
+outboards = neopixel.NeoPixel(board.A1, num_leds, brightness=0.5, auto_write=False)
 
 
 for i in range(num_leds):
@@ -50,13 +50,17 @@ while True:
     stamp_caterpillar(minute, 6, black, black)
     stamp_caterpillar(second, 3, black, black)
 
-    hour = put_value_into_pixels_range(t.tm_hour, 24, num_leds)
+    tmp_hour = t.tm_hour
+    if (tmp_hour >= 12):
+        tmp_hour = tmp_hour - 12
+
+    hour = put_value_into_pixels_range(tmp_hour, 12, num_leds)
     minute = put_value_into_pixels_range(t.tm_min, 60, num_leds)
     second = put_value_into_pixels_range(t.tm_sec, 60, num_leds)
     
-    stamp_caterpillar(hour, 10, (249, 124, 22), (247, 199, 27))
-    stamp_caterpillar(minute, 6, (0,0,255), (82, 143, 242))
-    stamp_caterpillar(second, 3, (0,255,0), (9, 2, 132))
+    stamp_caterpillar(hour, 9, (150, 23, 19), (249, 153, 17))
+    stamp_caterpillar(minute, 6, (2, 22, 242), (82, 143, 242))
+    stamp_caterpillar(second, 3, (255,255,255), (255, 255, 0))
     
     outboards.show()
     
